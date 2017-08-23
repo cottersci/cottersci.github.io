0114d684e28de4cd5fd8216600be5a2d13e94d89
Chris Cotter <cotter@sciencesundries.com>
2017-08-22 21:03:00 -0400

    Fixed publish script

--- publish.sh
+++ publish.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B master public upstream/master
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,5 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
+echo "Updating master branch"
 cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+
+echo "Pushing master branch"
+git push origin master

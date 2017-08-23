c59b0de9dd2da8d0d4f29359510928ae0793d591
Chris Cotter <cotter@sciencesundries.com>
2017-08-22 21:09:24 -0400

    Added creation of CNAME file

--- publish.sh
+++ publish.sh
@@ -21,8 +21,11 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+
 echo "Updating master branch"
-cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+cd public
+echo "www.sciencesundries.com" > CNAME
+git add --all && git commit -m "Publishing to master (publish.sh)"
 
 echo "Pushing master branch"
 git push origin master
